6d00abb52d0437e89b7767277df83fb9a585ac54
iceberg-work <iceberg-work@qq.com>
2022-02-25 12:40:28 +0800

    更新了头文件，删除了不必要的文件

---
+++ src/fileNameAndQuantity.h
@@ -0,0 +1,13 @@
+//
+// Created by iceberg-work on 2022/2/25.
+//
+
+#ifndef ACCOUNTINGASSISTANT_FILENAMEANDQUANTITY_H
+#define ACCOUNTINGASSISTANT_FILENAMEANDQUANTITY_H
+
+struct fileNameAndQuantity {
+    int fileQuantity;
+    char* fileName;
+};
+
+#endif //ACCOUNTINGASSISTANT_FILENAMEANDQUANTITY_H
